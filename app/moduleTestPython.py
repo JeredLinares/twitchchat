1a4243e4f38b60b637cc44b2600dc3bc9f40b6b2
Jered <JeredL@gmail.com>
2021-05-28 19:47:41 -0500

    twitch id added

---
+++ app/moduleTestPython.py
@@ -0,0 +1,5 @@
+## Test that python is working
+## Jered 
+## 5/28/21
+
+print("Hello World")
--- app/moduleTwitchAPI.py
+++ app/moduleTwitchAPI.py
@@ -1,5 +1,13 @@
-#
-#
+# Connect to the twitch api
+# JDLinares
+# May 28 2021
+
+
+# Print out the connection
+# https://dev.twitch.tv/docs/api/
+
+
+
 
 
 
